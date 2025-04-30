184f0c7092f321a28d19d45165c54338e0d03305
jitesh <jitesh.kumar05official@gmail.com>
2025-04-30 23:02:23 +0530

    the backend tut

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const morgan = require("morgan");
+
 const app = express();
 
-app.get('/', (req,res) =>{
-    res.send("HEllo world");
+app.use(morgan("dev"));
+app.set("view engine", "ejs");
+app.use((req,res,next) => {
+    console.log("This is middleware function");
+    let a = 10;
+    let b  = 20;
+    let c = a+b;
+    console.log("the sum of a and b is : " + c);
+    return next();
 })
+
+app.get('/', (req,res) =>{
+    console.log("Running successfully");
+    res.render("index");
+})
+
 app.get('/about',(req,res) =>{
     res.send('About page');
 })
+
 app.get('/contact',(req,res) =>{
     res.send("Contact page");
 })
